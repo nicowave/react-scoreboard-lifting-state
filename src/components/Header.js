d7fd7ade4aed595e95e32af72b7af7a7043370d0
nicowave <geoidlife@gmail.com>
2019-12-08 20:17:48 -0800

    add Stopwatch button onClick={ this.handleState } hook into class function, add button text change --with setState change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Stats from './Stats';
-
+import Stopwatch from './Stopwatch';
 
 const Header = (props) => {
   return (
@@ -8,8 +8,10 @@ const Header = (props) => {
       <Stats 
         players={props.players}
       />
-      <h1>{ props.title }</h1>      
+      <h1>{ props.title }</h1>    
+      <Stopwatch />  
     </header>
+ 
   );
 }
 
--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,12 +2,25 @@ import React, {Component} from 'react';
 
 // Application State or Component State
 class Stopwatch extends Component {
+
+  state = {
+    isRunning: false
+  }
+
+  handleStopwatch = () => {
+    this.setState({
+      isRunning: !this.state.isRunning
+    })
+  }
+
   render() {
     return(
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time"></span>
-        <button>Start</button>
+        <button onClick={ this.handleStopwatch }>
+          {this.state.isRunning ? 'Stop' : 'Start' }
+        </button>
         <button>Reset</button>
       </div>
     )
